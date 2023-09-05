bd76947485353fd6f7f35aff424abc25a0738281
Hassan Naeem <hassannaeem53@gmail.com>
2023-09-05 11:25:07 +0500

    Routes,Pages files added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './routes';
 
-
-function App() {
-  return <h1>Notification Client</h1>;
-}
+const App = () => (
+  <BrowserRouter>
+    <AppRouter />
+  </BrowserRouter>
+);
 
 export default App;
