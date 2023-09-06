e483882a335e4b02ad0c383407c4de745dada543
ZainabIftikhar1204 <ZainabIftikhar1204@gmail.com>
2023-09-06 19:03:13 +0500

    added carousel buttons functionality

--- src/containers/Application.tsx
+++ src/containers/Application.tsx
@@ -1,5 +1,5 @@
 import { ButtonGroup, CircularProgress, Grid, IconButton } from "@mui/material";
-// import { useState } from "react";
+import { useState } from "react";
 import Tile from "../components/Tile";
 import useData from "../hooks/useData";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
@@ -30,28 +30,39 @@ interface App {
 }
 
 const Application = () => {
-  // const [page, setPage] = useState<number>(1);
+  const [page, setPage] = useState<number>(1);
+  const pageSize = 4;
   const {
     data: apps,
     isLoading,
     error,
-  } = useData<App>("/apps", {
-    params: {
-      page_size: 4,
-      current_page: 2,
+  } = useData<App>(
+    "/apps",
+    {
+      params: {
+        page_size: pageSize,
+        current_page: page,
+      },
     },
-  });
+    [page]
+  );
+
+  const previousPage = () => page > 1 && setPage(page - 1);
+  const nextPage = () => {
+    apps.length === 4 && setPage(page + 1);
+  };
 
   if (isLoading) return <CircularProgress className="mx-auto" />;
 
   return (
     <>
       {error && <p>error</p>}
-      {apps.map((app, index) => (
-        <Grid item xs={12} md={3} style={{ display: "flex" }} key={index}>
-          <Tile appName={app.name} appDescription={app.description} />
-        </Grid>
-      ))}
+      {apps.length &&
+        apps.map((app, index) => (
+          <Grid item xs={12} md={3} style={{ display: "flex" }} key={index}>
+            <Tile appName={app.name} appDescription={app.description} />
+          </Grid>
+        ))}
       <Grid
         item
         xs={12}
@@ -61,10 +72,10 @@ const Application = () => {
         }}
       >
         <ButtonGroup>
-          <IconButton onClick={() => console.log("back arrow")}>
+          <IconButton onClick={previousPage} disabled={page === 1}>
             <ArrowBackIosIcon />
           </IconButton>
-          <IconButton onClick={() => console.log("forward arrow")}>
+          <IconButton onClick={nextPage} disabled={apps.length < pageSize}>
             <ArrowForwardIosIcon />
           </IconButton>
         </ButtonGroup>
